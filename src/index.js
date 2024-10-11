4173e9924607ea7dfd43ae04c71f2ee1559c7a81
dohun08 <bemik624@gmail.com>
2024-10-11 23:03:45 +0900

    style:main 긴문서 인기문서 퍼블리싱

--- src/index.js
+++ src/index.js
@@ -9,13 +9,16 @@ import {
 import Main from './page/main';
 import Login from './page/login';
 import Signup from './page/signup';
+import MoreContents from './page/moreContents';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <BrowserRouter>
         <Routes>
             <Route path={'/'} element={<Main />}></Route>
             <Route path={'/login'} element={<Login />}></Route>
             <Route path={'/signup'} element={<Signup />}></Route>
+            <Route path='/more' element={<MoreContents />}></Route>
         </Routes>
     </BrowserRouter>
 );
--- src/page/main/index.js
+++ src/page/main/index.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import * as S from './style.ts';
 import Header from "../../components/Header";
+import LongDocument from "../../components/Documents/Long";
+import Popular from "../../components/Documents/popular/index.js";
+import { Link } from "react-router-dom";
+
 function Main(){
+    const [isLogin, setIsLogin] = useState();
+    const getDocument = async ()=>{
+        
+    };
+    useEffect(()=>{
+        getDocument();
+    }, []);
     return(
-        <>
-            <Header />
-        </>
+        <S.container>
+            {/* isLogin에 받아온 isLogin 값 넣기 */}
+            <Header isLogin = {false}/>
+            <S.main>
+                <S.section2>
+                    <h3>인기 문서</h3>
+                    <Popular />
+                    
+                </S.section2>
+                <S.section1>  
+                    <LongDocument />
+                    
+                    <Link to={'/more'}>더보기</Link>
+                </S.section1>
+            </S.main>
+        </S.container>
     )
 }
 export default Main;
