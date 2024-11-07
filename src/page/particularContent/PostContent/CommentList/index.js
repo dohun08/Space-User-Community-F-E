c10dc9512f934d1fc8a9eec16742f189464ab846
sunz8281 <sunz8281@gmail.com>
2024-11-07 20:08:15 +0900

    add: Comment list 가져오기

--- src/page/particularContent/PostContent/CommentList/index.js
+++ src/page/particularContent/PostContent/CommentList/index.js
@@ -1,14 +1,38 @@
 import styled from "styled-components";
 import Comment from "./comment";
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
 
-export default function CommentList({comments}){
+export default function CommentList(){
+
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const [comments, setComments] = useState([]);
+    const getComments = async ()=>{
+        try{
+            const response = await fetch(`/community/comment/${id}`, {
+                method:'GET',
+            })
+            const data = await response.json();
+            if(data.status===200){
+                console.log("댓글 조회 성공");
+            }
+        }catch (error){
+            console.log("error on : ",error);
+            navigate("/error");
+        }
+    };
+
+    useEffect(()=>{
+        getComments();
+    }, []);
     return(
         <Ul>
             {comments.map((comment, index) => (
                 <>
                     <Comment
                         key={index}
-                        writer={comment.writer}
+                        writer={comment.userid}
                         date={comment.date}
                         content={comment.content}
                     />
