b5b1d1f6268a25ffb156ae529751e942d7313c10
dohun08 <bemik624@gmail.com>
2024-11-08 23:29:23 +0900

    refactor: popularDoc 반응형

--- src/components/Documents/popular/index.js
+++ src/components/Documents/popular/index.js
@@ -3,13 +3,10 @@ import * as S from './style.ts'
 
 function Popular(){
     return(
-        <>
         <S.content>
             <S.title>제목</S.title>
             <S.writer>저자</S.writer>
         </S.content>
-            
-        </>
     )
 }
 export default Popular;
