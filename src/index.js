a0eee4a88948ec2ce705bd73c6cd764065a7bb6b
dohun08 <bemik624@gmail.com>
2024-11-08 23:28:14 +0900

    add: search 페이지

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ import Signup from './page/signup';
 import MoreContents from './page/moreContents';
 import Write from './page/write';
 import Post from '../src/page/particularContent';
-
 import ReportManage from "./page/reportManage";
 import DetailedReport from "./page/detailedReport";
 import BanManage from "./page/banManage";
 import UserBan from './page/userBan';
 import Report from "./page/Report";
 import UserPage from "./page/UserPage";
+import Search from "./page/search";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -39,6 +39,7 @@ root.render(
                 <Route path={'/user/ban'} element={<UserBan />}></Route>
                 <Route path={'/report'} element={<Report />}></Route>
                 <Route path={'/user/:id'} element={<UserPage/>}></Route>
+                <Route path={'/search/:title'} element={<Search />}></Route>
             </Routes>
         </BrowserRouter>
     </RecoilRoot>
