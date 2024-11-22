e07f2b4528d93a5a7a07383f3c42bfd087eafca8
dohun08 <bemik624@gmail.com>
2024-11-22 10:19:16 +0900

    add : 댓글 지우기 기능

--- src/page/particularContent/PostContent/CommentList/comment.js
+++ src/page/particularContent/PostContent/CommentList/comment.js
@@ -1,11 +1,43 @@
 import styled from "styled-components";
+import {useRecoilValue} from "recoil";
+import {authAtom} from "../../../../recoil/authAtom";
 
-export default function Comment({writer, date, content}) {
+export default function Comment({writer, date, content, id, getComment}) {
+    const getAuth = useRecoilValue(authAtom);
+    const deleteComment = async () => {
+        try{
+            const res = await fetch(`/api/community/comment/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': getAuth.access_Token,
+                },
+                credentials: 'include'
+            })
+            if(res.ok){
+                getComment();
+            }
+        }catch (error){
+            console.log(error);
+        }
+    }
     return(
         <Wrapper>
             <TitleWrapper>
                 <Title>{writer} - {date}</Title>
-                <DelBtn>제거</DelBtn>
+                <span>
+                    {getAuth.username === writer ?
+                        <>
+                        <DelBtn type={"button"} value={"제거"} onClick={deleteComment}></DelBtn>
+                        <DelBtn type={"button"} value={"수정"} onClick={()=>console.log(1)}></DelBtn>
+                        </>:
+                        getAuth.isAdmin ?
+                                <DelBtn type={"button"} value={"제거"} onClick={deleteComment}></DelBtn>
+                             :
+                        null
+                    }
+
+                </span>
             </TitleWrapper>
             <Content>{content}</Content>
         </Wrapper>
@@ -31,10 +63,12 @@ const Title = styled.div`
     font-weight: 800;
 `
 
-const DelBtn = styled.div`
+const DelBtn = styled.input`
     cursor: pointer;
     color: #9A9A9A;
     font-size: 12px;
+    background: none;
+    border: none;
 `
 
 const Content = styled.div`
